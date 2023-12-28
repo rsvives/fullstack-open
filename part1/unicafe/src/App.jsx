01d45b473fac02d796db1be4057a8aa414426004
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-12-28 12:11:49 +0100

    ex 1.7 - added all, average and positive statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [score, setScore]=useState(0)
+  const [all, setAll]=useState(0)
 
   const addGood=()=>{
     setGood(good+1)
+    setAll(all+1)
+    setScore(score+1)
   }
   const addNeutral=()=>{
     setNeutral(neutral+1)
+    setAll(all+1)
+    setScore(score+0) //redundant but clearer
   }
   const addBad=()=>{
     setBad(bad+1)
+    setAll(all+1)
+    setScore(score-1)
   }
 
   return (
@@ -27,6 +35,9 @@ const App = () => {
       <p>good:  {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {all===0?0:score/all}</p>
+      <p>positive: {all===0?0:good*100/all}%</p>
     </div>
   )
 }
