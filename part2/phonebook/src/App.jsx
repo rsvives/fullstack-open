bf19f67dee96ae7f91cbc8a4e03d9717bd56010a
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-02 16:36:36 +0100

    ex 2.6 - adding persons to phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ id: 1, name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+  const addNewPerson = (ev) => {
+    ev.preventDefault()
+    console.log('addNewPerson')
+    const newPerson = {
+      id: persons.length + 1,
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={newName}
+            onChange={handleNewName}
+          />
+          <button type="submit" onClick={addNewPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      {/* <p>debug: {newName}</p> */}
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.id}>{person.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App
