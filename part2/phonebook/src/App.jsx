ede2b2ae329b91f66c3b28ad9edc4e306b50848a
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-02 17:45:32 +0100

    ex 2.9 - filtering persons by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
@@ -16,7 +17,14 @@ const App = () => {
 
   const addNewPerson = (ev) => {
     ev.preventDefault()
-    // console.log('addNewPerson')
+
+    // check if name or number is empty
+    if (newName === '' || newNumber === '') {
+      alert('Please fill in the fields')
+      return
+    }
+
+    // check if name already exists
     const duplicates = persons.find((person) => person.name === newName)
 
     if (duplicates) {
@@ -34,44 +42,64 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleNameFilter = (ev) => {
+    setFilterName(ev.target.value)
+  }
+
+  const filteredPersons =
+    filterName === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filterName.toLowerCase())
+        )
+
   return (
     <div>
       <h1>Phonebook</h1>
+      <h2>Add new</h2>
       <form>
         <div>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <br />
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={newName}
-              onChange={handleNewName}
-            />
-          </div>
-          <div>
-            <label htmlFor="number">Number:</label>
-            <br />
-            <input
-              type="phone"
-              id="number"
-              name="number"
-              value={newNumber}
-              onChange={handleNewNumber}
-              pattern="[0-9]{9}"
-            />
-          </div>
-          <button type="submit" onClick={addNewPerson}>
-            add
-          </button>
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={newName}
+            onChange={handleNewName}
+          />
         </div>
+        <div>
+          <label htmlFor="number">Number:</label>
+          <br />
+          <input
+            type="phone"
+            id="number"
+            name="number"
+            value={newNumber}
+            onChange={handleNewNumber}
+            pattern="[0-9]{9}"
+          />
+        </div>
+        <button type="submit" onClick={addNewPerson}>
+          add
+        </button>
       </form>
-      {/* <p>debug: {newName}</p> */}
+      <h2>Look for someone</h2>
+      <label htmlFor="filter-name">Show people named: </label>
+      <input
+        id="filter-name"
+        type="text"
+        value={filterName}
+        onChange={handleNameFilter}
+      />
+
+      <p>debug: {filterName}</p>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.id}>
-          {person.name}: {person.number}
+
+      {filteredPersons.map((p) => (
+        <div key={p.id}>
+          {p.name}: {p.number}
         </div>
       ))}
     </div>
