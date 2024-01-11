aa2ec7246ff4f1396d3060e503bac40f29a78da9
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-11 23:39:47 +0100

    ex 3.8 - removed console.log

--- part3/phonebook_api/index.js
+++ part3/phonebook_api/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 morgan.token("body", function getBody(req) {
-  console.log("req", req.method);
+  // console.log("req", req.method);
   return JSON.stringify(req.body);
 });
 
