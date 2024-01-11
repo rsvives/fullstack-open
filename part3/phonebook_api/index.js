17794356a63ab520660aefcd8cee1a3a2d803e99
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-11 23:21:42 +0100

    ex 3.7 - morgan middleware tiny

--- part3/phonebook_api/index.js
+++ part3/phonebook_api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan("tiny"));
 
 const MAX_ID = Math.pow(2, 53);
 
