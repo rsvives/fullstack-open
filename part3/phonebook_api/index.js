c415c8f821d16333f95cfa8d567b884f66f55ae3
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-11 14:49:48 +0100

    Ex 3.2 - added info page on /info

--- part3/phonebook_api/index.js
+++ part3/phonebook_api/index.js
@@ -31,6 +31,13 @@ app.get("/api/persons", (req, res) => {
   res.status(200).json(persons);
 });
 
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
