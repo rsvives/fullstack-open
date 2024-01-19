7a2b8bad80bf266df344008a4f6e41c3554cc55d
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-19 10:26:44 -0800

    mongodb

--- part3/lecture/index.js
+++ part3/lecture/index.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose')
+
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+
+const mongoUri = `mongodb+srv://rsvives:${process.env.MONGO_PW}@cluster0.8yvecow.mongodb.net/noteApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false)
+mongoose.connect(mongoUri)
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -20,23 +27,47 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-let notes = [
-  {
-    id: 1,
-  content: "HTML is easy",
-  important: true,
-  },
-  {
-    id: 2,
-  content: "Browser can execute only JavaScript",
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'programming is fun',
   important: false,
-  },
-  {
-    id: 3,
-  content: "GET and POST are the most important methods of HTTP protocol",
-  important: true,
-  },
-];
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+
+Note.find({important:false}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// let notes = [
+//   {
+//     id: 1,
+//   content: "HTML is easy",
+//   important: true,
+//   },
+//   {
+//     id: 2,
+//   content: "Browser can execute only JavaScript",
+//   important: false,
+//   },
+//   {
+//     id: 3,
+//   content: "GET and POST are the most important methods of HTTP protocol",
+//   important: true,
+//   },
+// ];
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
