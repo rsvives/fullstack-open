035e1f0c4515c878565b869dd7f484daeb76c6b2
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-20 10:24:50 -0800

    ex 3.22 - linter config and fixing errors

--- part3/phonebook_api/index.js
+++ part3/phonebook_api/index.js
@@ -1,120 +1,117 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const Person = require('./models/person')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token("body", function getBody(req) {
+morgan.token('body', function getBody (req) {
   // console.log("req", req.method);
-  return JSON.stringify(req.body);
-});
-app.use(morgan("tiny"));
+  return JSON.stringify(req.body)
+})
+app.use(morgan('tiny'))
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :body",
+    ':method :url :status :res[content-length] - :response-time ms :body',
     {
-      skip(req, res) {
-        return req.method !== "POST";
-      },
+      skip (req, res) {
+        return req.method !== 'POST'
+      }
     }
   )
-);
-
-
-
-
+)
 
 // index
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send(
-    `<h1>Phonebook app</h1>`
-  );
-});
+    '<h1>Phonebook app</h1>'
+  )
+})
 
 // all persons
-app.get("/api/persons", (req, res, next) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
-  .then(persons=>{
-    res.status(200).json(persons);
-  }).catch(error=>next(error))
-});
+    .then(persons => {
+      res.status(200).json(persons)
+    }).catch(error => next(error))
+})
 
 // find person by id
-app.get("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
-  Person.findOne({_id:id})
-  .then(person=>{
-    if (person) {
-      res.status(200).json(person);
-    } else {
-      res.status(404).end();
-    }
-  }).catch((error)=>next(error))
-});
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+  Person.findOne({ _id: id })
+    .then(person => {
+      if (person) {
+        res.status(200).json(person)
+      } else {
+        res.status(404).end()
+      }
+    }).catch((error) => next(error))
+})
 
 // delete person
-app.delete("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
 
-      Person.findByIdAndDelete(id)
-      .then((person)=>{  
-        if(person){
-          res.status(204).end()
-        }else{
-          throw new Error('deleting error: person not found')
-        }
-      }).catch((error)=>{
-        next(error)
-      })
-});
+  Person.findByIdAndDelete(id)
+    .then((person) => {
+      if (person) {
+        res.status(204).end()
+      } else {
+        throw new Error('deleting error: person not found')
+      }
+    }).catch((error) => {
+      next(error)
+    })
+})
 
 // new person
-app.post("/api/persons", (req, res,next) => {
+app.post('/api/persons', (req, res, next) => {
   // if (!req.body.name || !req.body.number) {
-    // throw new Error("content missing")
+  // throw new Error("content missing")
   // } else {
-    Person.find( {name:req.body.name})
-    .then((result)=>{
+  Person.find({ name: req.body.name })
+    .then((result) => {
       // console.log(result)
       if (!result.length) {
-        const person = new Person({...req.body})      
-        person.save().then((savedPerson)=>{
+        const person = new Person({ ...req.body })
+        person.save().then((savedPerson) => {
           // throw new Error()
-          res.status(201).json({ message: `${savedPerson.name} was added`, person:savedPerson })
+          res.status(201).json({ message: `${savedPerson.name} was added`, person: savedPerson })
         })
-        .catch(error=>next(error))
+          .catch(error => next(error))
       } else {
-        throw new Error("name must be unique")
+        throw new Error('name must be unique')
       }
     })
-    .catch((error)=>next(error))
+    .catch((error) => next(error))
 
   // }
-});
+})
 
-//update
-app.put("/api/persons/:id", (req, res,next) => {
-  console.log("put method");
-  const id = req.params.id;
-  Person.findByIdAndUpdate(id,{...req.body},{new:true, runValidators:true,context:'query'})
-  .then(updatedPerson => {
+// update
+app.put('/api/persons/:id', (req, res, next) => {
+  console.log('put method')
+  const id = req.params.id
+  Person.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
       res.json(updatedPerson)
     })
     .catch(error => next(error))
-
-});
+})
 
 // info
-app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}</p>`
-  )
+app.get('/info', (req, res) => {
+  Person.find({}).then(persons => {
+    res.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${Date()}</p>`
+    )
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -124,33 +121,31 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  switch(error.name) {
+  switch (error.name) {
     case 'CastError':
       return response.status(400).send({ error: 'malformatted id' })
     case 'ValidationError':
-      return response.status(400).send({error:error.message})
+      return response.status(400).send({ error: error.message })
   }
 
-  
-  switch(error.message){
+  switch (error.message) {
     case 'content missing':
-      return response.status(400).send({error: error.message} )
+      return response.status(400).send({ error: error.message })
     case 'name must be unique':
-      return response.status(409).send({error: error.message} )
+      return response.status(409).send({ error: error.message })
     case 'error saving new person':
-      return response.status(400).send({error: error.message} )
+      return response.status(400).send({ error: error.message })
     case 'deleting error: person not found':
-      return response.status(404).send({error:error.message})
+      return response.status(404).send({ error: error.message })
     default:
-    next(error)
-  } 
+      next(error)
+  }
 }
-
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook_api/models/person.js
+++ part3/phonebook_api/models/person.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
-    .then((res)=>{
-        console.log('connected to MongoDB');
-    })
-    .catch(err=>{
-        console.error('could not connect to MongoDB', err);
-    })
+  .then((res) => {
+    console.log('connected to MongoDB')
+  })
+  .catch(err => {
+    console.error('could not connect to MongoDB', err)
+  })
 
 // Schema
 const personSchema = new mongoose.Schema({
@@ -21,10 +21,10 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    validate:{
-        validator: function(v) {
-            return /(\d{2}-\d{6,})|(\d{3}-\d{5,})/.test(v);
-        },
+    validate: {
+      validator: function (v) {
+        return /(\d{2}-\d{6,})|(\d{3}-\d{5,})/.test(v)
+      },
       message: props => `${props.value} is not a valid phone number! Correct format: 12-123456 or 123-12345`
     },
     required: [true, 'User phone number required']
@@ -32,13 +32,13 @@ const personSchema = new mongoose.Schema({
 })
 
 // Transform toJSON
-personSchema.set('toJSON',{
-    transform: (document, returnedObject)=>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-//Export
-module.exports = mongoose.model('Person',personSchema)
+// Export
+module.exports = mongoose.model('Person', personSchema)
