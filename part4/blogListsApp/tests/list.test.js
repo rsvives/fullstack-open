7a14074825a71938e6ac7db51a7397018c04d0ce
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-25 16:10:21 -0800

    ex 4.4 totalLikes function and test

--- part4/blogListsApp/tests/list.test.js
+++ part4/blogListsApp/tests/list.test.js
@@ -6,3 +6,58 @@ test('dummy', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list is empty, equals 0', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  const listWithBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('when list has many blogs, equals the sum of all of them', () => {
+    const result = listHelper.totalLikes(listWithBlogs)
+    expect(result).toBe(24)
+  })
+})
--- part4/blogListsApp/utils/list_helper.js
+++ part4/blogListsApp/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return blogs ? 1 : 0
 }
 
+const totalLikes = (list) => {
+  return list.reduce((prev, current) => prev + current.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
