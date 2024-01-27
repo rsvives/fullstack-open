895518e6bd8098fd608f4a318feb3f0ad8a46a3c
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-01-26 19:21:03 -0800

    ex 4.8 - initializing db with helper

--- part4/blogListsApp/tests/blog_list_api.test.js
+++ part4/blogListsApp/tests/blog_list_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (const blog of helper.blogs) {
+    const newBlog = new Blog(blog)
+    await newBlog.save()
+  }
+})
 
 describe('getting blogs from db', () => {
   test('notes are returned in JSON', async () => {
