11c41427d40772ba7c699637356414c5606b88b4
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-02-01 07:26:03 -0800

    cleaned comments

--- part5/bloglist-frontend/src/components/NotificationToast.jsx
+++ part5/bloglist-frontend/src/components/NotificationToast.jsx
@@ -1,17 +1,5 @@
 import './NotificationToast.css'
 const NotificationToast = ({ message, status }) => {
-  // const style = {
-  //   toast: {
-  //     padding: 12,
-  //     display: 'flex',
-  //     border: 1,
-  //     color: 'blue'
-  //   },
-  //   error: {
-  //     border: 'red',
-  //     color: 'red'
-  //   }
-  // }
   return (
         <div className={`toast ${status}`}>{message}</div>
   )
