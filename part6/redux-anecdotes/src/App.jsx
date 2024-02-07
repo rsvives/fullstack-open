ccc5b5c6d48ba25a0330e097e85a9f24d202f245
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-02-06 18:52:43 -0800

    ex 6.5 - sorted by votes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
 
   const vote = (id) => {
