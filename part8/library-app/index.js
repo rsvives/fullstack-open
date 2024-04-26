469448da28683e257c1d6de4ce3607e07bf1c83b
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-04-27 00:16:33 +0200

    ex 8.4 - books by author

--- part8/library-app/index.js
+++ part8/library-app/index.js
@@ -101,7 +101,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -126,7 +126,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount:()=>authors.length,
-    allBooks: ()=> books,
+    allBooks: (root,args)=> {
+        if(!args.author){
+            return books
+        }
+        const writtenBooks = books.filter(el=>el.author===args.author)
+        return writtenBooks
+    },
     allAuthors: ()=>authors
   },
   Author: {
