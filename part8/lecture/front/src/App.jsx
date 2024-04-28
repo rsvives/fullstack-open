f5adf191537c1e66b2eada9242dc5de2324faeff
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-04-28 13:42:16 +0200

    cleaning

--- part8/lecture/front/src/App.jsx
+++ part8/lecture/front/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import {useState } from 'react'
 import './App.css'
 import{ useQuery, useMutation} from '@apollo/client'
 import {ALL_PERSONS,FIND_BY_NAME, ADD_PERSON, EDIT_NUMBER} from './queries.js'
@@ -6,9 +6,6 @@ import {ALL_PERSONS,FIND_BY_NAME, ADD_PERSON, EDIT_NUMBER} from './queries.js'
 function App() {
 
   const [add_person] = useMutation(ADD_PERSON,{refetchQueries:[{query:ALL_PERSONS}]})
-
-
-  useEffect
 
   const result = useQuery(ALL_PERSONS)
 // const result = useQuery(FIND_BY_NAME,{variables:{nameToSearch:"Arto Hellas"}})
